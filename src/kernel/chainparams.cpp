ef429f8355dc23a3cdc4608fd72899de2beacfb4
quantanetwork-project <takeshinakamoto7@gmail.com>
2025-06-07 10:34:45 +0530

    seed

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -142,7 +142,6 @@ public:
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
         vSeeds.emplace_back("seed1.quantanetwork.org");
-        vSeeds.emplace_back("dnsseed.quantanetwork.org");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,58);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,125);
